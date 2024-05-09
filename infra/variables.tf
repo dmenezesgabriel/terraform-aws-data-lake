27df17a4e1bc5998615838fcaf070ca6556e1f6b
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-05-08 21:23:11 -0300

    feat: add glue crawler trigger lambda

--- infra/variables.tf
+++ infra/variables.tf
@@ -7,3 +7,9 @@ variable "bucket_list" {
   type    = list(string)
   default = ["sor", "sot", "spec"]
 }
+
+variable "lambda_policy_arn" {
+  description = "ARN of the IAM role policy to attach to the lambda role."
+  type        = string
+  default     = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
