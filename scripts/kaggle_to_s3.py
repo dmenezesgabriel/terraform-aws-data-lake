30026a6cfe36cb42ac27fb07677cde0bd37c1b4a
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2024-05-15 22:42:59 -0300

    refactor: kaggle_to_s3 script to enable crawler cataloging

--- scripts/kaggle_to_s3.py
+++ scripts/kaggle_to_s3.py
@@ -1,5 +1,10 @@
+import logging
 import os
+from pathlib import Path
 from zipfile import ZipFile
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 import boto3
 from kaggle.api.kaggle_api_extended import KaggleApi
@@ -20,18 +25,22 @@ def download_dataset_and_upload(dataset_slug, s3_bucket_name, s3_prefix):
         zip_ref.extractall("data")
 
     for file in os.listdir("data"):
+        logger.info(f"Uploading file: {file}")
         if os.path.isfile(os.path.join("data", file)):
+            file_path = os.path.join("data", file)
+            file_name = Path(file).stem
+            s3_key = os.path.join(s3_prefix, file_name, file)
             upload_to_s3(
-                os.path.join("data", file),
+                file_path,
                 s3_bucket_name,
-                os.path.join(s3_prefix, file),
+                s3_key,
             )
 
 
 if __name__ == "__main__":
     sts_client = boto3.client("sts")
     account_id = sts_client.get_caller_identity().get("Account")
-    dataset_slug = "jaidalmotra/pokemon-dataset"
+    dataset_slug = "rishabjadhav/imdb-actors-and-movies"
     s3_bucket_name = f"sor-{account_id}"
-    s3_prefix = "pokemon-dataset"
+    s3_prefix = dataset_slug.split("/")[1]
     download_dataset_and_upload(dataset_slug, s3_bucket_name, s3_prefix)
